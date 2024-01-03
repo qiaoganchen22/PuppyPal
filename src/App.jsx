277812511233e3ce5276e822cc4a5f4f89f61c48
Qiao Gan Chen <qiaoganchen@gmail.com>
2024-01-03 15:39:36 -0500

    commit 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-// import "./App.css";
+import "./App.css";
+import {puppyList} from './data.js'
 
 function App() {
-  // const [count, setCount] = useState(0);
-
+  const [puppies, setPuppies] = useState(puppyList);
+console.log(puppies)
 
   return (
       <div>
-        <h1>ddssdsasddsafgwerwer</h1>
+        {
+          puppies.map((puppy)=>{
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
   )
 }
