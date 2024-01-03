357329aa7b9c2355842a0cf10f513df4a2dfd903
Qiao Gan Chen <qiaoganchen@gmail.com>
2024-01-03 16:22:00 -0500

    commit2

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,25 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
       <div>
+        {featPupId && (
+        <div className="info Box"> 
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
         {
           puppies.map((puppy)=>{
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <div className="flex"><p className="pups"onClick={()=>{
+              setFeatPupId(puppy.id)
+            }} key={puppy.id}>{puppy.name}</p></div>
           })
         }
       </div>
